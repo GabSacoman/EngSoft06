e91339e55198562308489402bc6b47358feb9030
gabsacoman <gabriel.sacoman@hotmail.com>
2022-11-23 14:12:17 -0300

    remove coment

--- toUpperCase.test.js
+++ toUpperCase.test.js
@@ -1,5 +1,5 @@
 import changeToUpperCase from './toUpperCase.js'
-//verifica se está em Maiusculo 
+
 describe('Verify toUpperCase', () => {
     it('hello to HELLO', () => {
         const retornoEsperado = 'HELLO';
